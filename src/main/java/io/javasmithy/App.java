b5933229f6b1f65d71d073c03e2d907ff0269347
peter <peter.k.hydesmith@gmail.com>
2020-02-11 17:46:55 -0600

    Basic GUI in main that works

--- src/main/java/io/javasmithy/App.java
+++ src/main/java/io/javasmithy/App.java
@@ -2,12 +2,22 @@ package io.javasmithy;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
 public class App extends Application{
 
     @Override
     public void start(Stage stage){
-        
+        Scene scene = new Scene(
+            new AnchorPane(
+                new Button("BUTTON")
+            ),
+            500,
+            500
+        );
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main( String[] args )
