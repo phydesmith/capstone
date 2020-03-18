5e5404288b2e171a7b0b9d1c6e647c1cf92f4e8d
Peter <me@me>
2020-03-17 23:59:47 -0500

    small change in Gnome description

--- src/main/java/io/javasmithy/model/component/race/Race.java
+++ src/main/java/io/javasmithy/model/component/race/Race.java
@@ -4,7 +4,7 @@ public enum Race{
     DRAGONBORN("Dragonborn", "Half-Man, Half-Dragon, charismatic with an affinity towards sorcery."),
     DWARF("Dwarf", "Gruff and stout. Militant and industrious tendancies"),
     ELF("Elf", "Graceful and feylike forest dwellers."),
-    GNOME("Gnome", "Short and stocky. Tend towards inquisitiveness, curiosity and electivism."),
+    GNOME("Gnome", "Short and stocky. Tend towards inquisitiveness, curiosity and eclecticism."),
     HALFLING("Halfling", "Short and slight. Simple agrarian lives centered around hearth and harvest. Some have an adventurous streak."),
     HALFELF("Half-Elf", "The best features of humans and elves- but the ability to fit in with neither fully. Tend to be restless and long for adventure."),
     HALFORC("Half-Orc", "Large and strong. Value strength and tend to be mercenaries or soliders."),
