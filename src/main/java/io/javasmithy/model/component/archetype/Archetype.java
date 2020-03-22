7af3bfcea7bbe5e59649772ef6a8fad4b82f3246
Peter <me@me>
2020-03-22 02:08:04 -0500

    archetypes updated with skills and saving throws

--- src/main/java/io/javasmithy/model/component/archetype/Archetype.java
+++ src/main/java/io/javasmithy/model/component/archetype/Archetype.java
@@ -1,19 +1,53 @@
 package io.javasmithy.model.component.archetype;
 
+import io.javasmithy.model.component.ability.Ability;
+import io.javasmithy.model.component.skill.Skill;
+
+
+import java.util.List;
+
 public enum Archetype {
-    CLERIC("Cleric", "A priestly champion who wields divine magic in service of a higher power", 8),
-    FIGHTER("Fighter", "A master of martial combat, skilled with a variety of weapons and armor", 10),
-    WIZARD("Wizard", "A scholarly magic-user capable of manipulating the structures of reality", 6),
-    ROGUE("Rogue", "A scoundrel who uses stealth and trickery to overcome obstacles and enemies", 8);
+    CLERIC("Cleric", "A priestly champion who wields divine magic in service of a higher power", 8, 
+            List.of(Ability.WISDOM, Ability.CHARISMA), 
+            List.of(Skill.HISTORY, Skill.INSIGHT, Skill.MEDICINE, Skill.PERSUASION, Skill.RELIGION)
+    ),
+    FIGHTER("Fighter", "A master of martial combat, skilled with a variety of weapons and armor", 10,
+            List.of(Ability.STRENGTH, Ability.CONSTITUTION), 
+            List.of(Skill.ACROBATICS, Skill.ANIMAL_HANDLING, Skill.ATHLETICS, Skill.HISTORY, Skill.INSIGHT, Skill.INTIMIDATION, Skill.PERCEPTION, Skill.SURVIVAL)
+    ),
+    ROGUE("Rogue", "A scoundrel who uses stealth and trickery to overcome obstacles and enemies", 8,
+        List.of(Ability.DEXTERITY , Ability.INTELLIGENCE), 
+        List.of(Skill.ACROBATICS, 
+                Skill.ATHLETICS, 
+                Skill.DECEPTION, 
+                Skill.INSIGHT, 
+                Skill.INTIMIDATION,
+                Skill.INVESTIGATION, 
+                Skill.PERCEPTION,
+                Skill.PERFORMANCE,
+                Skill.PERSUASION,
+                Skill.SLEIGHT_OF_HAND,
+                Skill.STEALTH)
+    ),
+    WIZARD("Wizard", "A scholarly magic-user capable of manipulating the structures of reality", 6,
+        List.of(Ability.INTELLIGENCE, Ability.WISDOM), 
+        List.of(Skill.ARCANA, Skill.HISTORY, Skill.INSIGHT, Skill.INVESTIGATION,  Skill.MEDICINE, Skill.RELIGION)
+    );
+
     
     private String name;
     private String description;
     private int hitDie;
 
-    private Archetype(String name, String description, int hitDie){
+    private List<Ability> proficientSavingThrows;
+    private List<Skill> skills;
+
+    private Archetype(String name, String description, int hitDie, List<Ability> savingThrows, List<Skill> skills){
         this.name = name;
         this.description = description;
         this.hitDie = hitDie;
+        this.proficientSavingThrows = savingThrows;
+        this.skills = skills;
     }
 
     public String getName() {
@@ -24,6 +58,12 @@ public enum Archetype {
     }
     public int getHitDie() {
         return hitDie;
+    }
+    public List<Ability> getProficientSavingThrows(){
+        return this.proficientSavingThrows;
+    }
+    public List<Skill> getSkillList(){
+        return this.skills;
     }
 
     public String toString(){
--- src/main/java/io/javasmithy/model/component/skill/Skill.java
+++ src/main/java/io/javasmithy/model/component/skill/Skill.java
@@ -1,33 +1,57 @@
 package io.javasmithy.model.component.skill;
 
+import io.javasmithy.model.component.ability.Ability;
+
 public enum Skill{
     //Strength
-    ATHLETICS,
+    ATHLETICS("Athletics", " desc placeholder", Ability.STRENGTH),
 
     //Dexterity
-    ACROBATICS,
-    SLEIGHT_OF_HAND,
-    STEALTH,
+    ACROBATICS("Acrobatics", " desc placeholder", Ability.DEXTERITY),
+    SLEIGHT_OF_HAND("Sleight of Hand", " desc placeholder", Ability.DEXTERITY),
+    STEALTH("Stealth", " desc placeholder", Ability.DEXTERITY),
 
     //Intelligence
-    ARCANA,
-    HISTORY,
-    INVESTIGATION,
-    NATURE,
-    RELIGION,
+    ARCANA("Arcana", " desc placeholder", Ability.INTELLIGENCE),
+    HISTORY("History", " desc placeholder", Ability.INTELLIGENCE),
+    INVESTIGATION("Investigation", " desc placeholder", Ability.INTELLIGENCE),
+    NATURE("Nature", " desc placeholder", Ability.INTELLIGENCE),
+    RELIGION("Religion", " desc placeholder", Ability.INTELLIGENCE),
 
     //Wisdom
-    ANIMAL_HANDLING,
-    INSIGHT,
-    MEDICINE,
-    PERCEPTION,
-    SURVIVAL,
+    ANIMAL_HANDLING("Animal Handling", " desc placeholder", Ability.WISDOM),
+    INSIGHT("Insight", " desc placeholder", Ability.WISDOM),
+    MEDICINE("Medicine", " desc placeholder", Ability.WISDOM),
+    PERCEPTION("Perception", " desc placeholder", Ability.WISDOM),
+    SURVIVAL("Survival", " desc placeholder", Ability.WISDOM),
 
     //Charisma
-    DECEPTION,
-    INTIMIDATION,
-    PERFORMANCE,
-    PERSUASION;
+    DECEPTION("Deception", " desc placeholder", Ability.CHARISMA),
+    INTIMIDATION("Intimidation", " desc placeholder", Ability.CHARISMA),
+    PERFORMANCE("Performance", " desc placeholder", Ability.CHARISMA),
+    PERSUASION("Persuasion", " desc placeholder", Ability.CHARISMA);
 
+    private String name;
+    private String description;
+    private Ability ability;
 
+    private Skill(String name, String description, Ability ability){
+        this.name = name;
+        this.description = description;
+        this.ability = ability;
+    }
+
+    public Ability getAbility(){
+        return this.ability;
+    }
+    public String getName(){
+        return this.name();
+    }
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String toString(){
+        return this.name;
+    }
 }
