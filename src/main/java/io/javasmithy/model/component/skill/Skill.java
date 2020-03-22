9688604cbeefc98c5097ea9da4c5e840d250b3e6
Peter <me@me>
2020-03-21 21:07:16 -0500

    added skill enums

---
+++ src/main/java/io/javasmithy/model/component/skill/Skill.java
@@ -0,0 +1,33 @@
+package io.javasmithy.model.component.skill;
+
+public enum Skill{
+    //Strength
+    ATHLETICS,
+
+    //Dexterity
+    ACROBATICS,
+    SLEIGHT_OF_HAND,
+    STEALTH,
+
+    //Intelligence
+    ARCANA,
+    HISTORY,
+    INVESTIGATION,
+    NATURE,
+    RELIGION,
+
+    //Wisdom
+    ANIMAL_HANDLING,
+    INSIGHT,
+    MEDICINE,
+    PERCEPTION,
+    SURVIVAL,
+
+    //Charisma
+    DECEPTION,
+    INTIMIDATION,
+    PERFORMANCE,
+    PERSUASION;
+
+
+}
