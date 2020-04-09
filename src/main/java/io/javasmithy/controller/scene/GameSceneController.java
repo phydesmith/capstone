7187a6276802d4255b4dd3e0910dd0dfa2ce8a64
Peter <me@me>
2020-04-09 11:43:48 -0500

    small change before branch

--- src/main/java/io/javasmithy/controller/scene/GameSceneController.java
+++ src/main/java/io/javasmithy/controller/scene/GameSceneController.java
@@ -25,6 +25,7 @@ public class GameSceneController implements Initializable, SceneController {
     private GameController gc;
     private Scene menuScene;
     private String[] panePaths = {"fxml/Room1.fxml", "fxml/Room2.fxml", "fxml/Room3.fxml"};
+    private String[] roomConfig = {"room1.json", "room2.json", "room3.json"};
 
     // testing
     int gCtr = 0;
